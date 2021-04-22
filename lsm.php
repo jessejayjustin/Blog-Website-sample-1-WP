e8872b9a63f19188899243dec8fe178baf4f1a83
Jesse Sosmena <23520011+jessejayjustin@users.noreply.github.com>
2021-04-22 14:15:08 +0800

    first commit

---
+++ lsm.php
@@ -0,0 +1,33 @@
+<div class="_lsm-container clearfix">
+	<div class="_lsm-row">
+		<h2 class="text-center">Links and Social Media</h2>
+        <div class="_lsm-column text-center">
+        	<ul class="col-lg-8">
+        		<li>
+        			<a href="#"><img src="<?php echo(get_template_directory_uri()); ?>/images/facebook.png" alt="Facebook logo" style="width:60px;"></a>
+        		</li>
+        		<li>
+        			<a href="#"><img src="<?php echo(get_template_directory_uri()); ?>/images/googleplus.png" alt="Google Plus logo" style="width:60px;"></a>
+        		</li>
+        		<li>
+        			<a href="#"><img src="<?php echo(get_template_directory_uri()); ?>/images/twitter.jpg" alt="Twitter logo" style="width:60px;"></a>
+        		</li>
+        		<li>
+        			<a href="#"><img src="<?php echo(get_template_directory_uri()); ?>/images/google.jpg" alt="Google logo" style="width:60px;"></a>
+        		</li>
+        		<li>
+        			<a href="#"><img src="<?php echo(get_template_directory_uri()); ?>/images/pinterest.jpg" alt="Pinterest logo" style="width:60px;"></a>
+        		</li>
+        		<li>
+        			<a href="#"><img src="<?php echo(get_template_directory_uri()); ?>/images/romance.jpg" alt="Romance writers logo" style="width:110px;"></a>
+        		</li>
+        		<li>
+        			<a href="#"><img src="<?php echo(get_template_directory_uri()); ?>/images/specficnz.jpg" alt="Specficnz logo" style="width:90px;"></a>
+        		</li>
+        	</ul>
+        </div>
+    	<div class="_lsm-btn-sec">
+			<input class="btn btn-default _lsm-btn" type="button" value="VIEW FULL LINES">
+		</div>
+	</div>
+</div><br><br><br>
